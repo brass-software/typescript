d34083d5f498c20136bbef192e05f80a4b96adb9
Mike Rybka <merybka@gmail.com>
2024-03-06 10:36:58 -0500

    Create ImportMap type

---
+++ import_map.go
@@ -0,0 +1,27 @@
+package typescript
+
+import (
+	"fmt"
+
+	"github.com/brass-software/nodejs"
+)
+
+type ImportMap struct {
+	Imports []*nodejs.Import
+}
+
+func (m *ImportMap) Add(i *nodejs.Import) error {
+	for _, imp := range m.Imports {
+		if imp.From == i.From {
+			if i.Default != "" {
+				if imp.Default != "" && imp.Default != i.Default {
+					return fmt.Errorf("default import %s already exists from %s", i.Default, i.From)
+				}
+				imp.Default = i.Default
+			}
+			return nil
+		}
+	}
+	m.Imports = append(m.Imports, i)
+	return nil
+}
